c82c989ace5ff4af587773351fab37ec82788ea7
goodperson99 <tcimermonas@gmail.com>
2014-03-13 23:39:07 +0200

    added bg added buttons Allright, added new activities, new buttons, everything is new. Feel free updating it. Use the same background

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/reportBoth.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/reportBoth.java
@@ -1,6 +1,5 @@
 package pl.byd.wsg.promand.project1;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,26 +10,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-    Button button1,btn2;
+public class reportBoth extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.graphsOfBothBtn);
-        btn2 = (Button) findViewById(R.id.reportsOfBothBtn);
-        button1.setOnClickListener(this);
+        setContentView(R.layout.activity_report_both);
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
+        
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.report_both, menu);
         return true;
     }
 
@@ -45,15 +45,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
         return super.onOptionsItemSelected(item);
     }
-    private void onGraphBothClick(){
-        startActivity(new Intent("pl.byd.wsg.promand.project1.graphBoth"));
-    }
-    @Override
-    public void onClick(View view) {
-        switch ((view.getId())){
-            case R.id.graphsOfBothBtn:
-                onGraphBothClick();
-                break;
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_report_both, container, false);
+            return rootView;
         }
     }
+
 }
