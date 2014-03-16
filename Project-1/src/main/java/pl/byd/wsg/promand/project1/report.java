07c8db5520e35379eb6958001b293b8c03b72d2d
Fred <fred.mihkelsoo@gmail.com>
2014-03-17 01:19:06 +0200

    Added new logos and app name

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/report.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/report.java
@@ -26,13 +26,13 @@ public class report extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.report, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.report, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
